b2d0342512c95dd3121a47526b3333b0b4912f9b
Barto <ponsbarto@gmail.com>
2024-02-18 16:17:18 +0100

    implementar cambiops eb home y home css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,71 +1,11 @@
-import { useEffect, useState } from "react";
 import "./Home.css";
-import { LoginInput } from "../../components/LoginInput/LoginInput";
-import { bringAllCharacters } from "../../services/apiCalls";
-import { ArtistCard } from "../../components/ArtistCard/ArtistCard";
 
 export const Home = () => {
-  const [characters, setCharacters] = useState([]);
-  const [userData, setUserData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const inputHandler = (event) => {
-    setUserData((prevState) => ({
-      ...prevState,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
-  const buttonHamdler = () => {
-    bringAllCharacters().then((characters) => {
-      setCharacters(characters);
-    });
-  };
-
-  useEffect(() => {
-    console.log(characters);
-  }, [characters]);
-
-  useEffect(() => {
-    //console.log(userData)
-  }, [userData]);
-
   return (
-    <div className="miDiv">
-      <LoginInput
-        type={"text"}
-        name={"name"}
-        handler={inputHandler}
-      ></LoginInput>
-      <LoginInput
-        type={"email"}
-        name={"email"}
-        handler={inputHandler}
-      ></LoginInput>
-      <LoginInput
-        type={"password"}
-        name={"password"}
-        handler={inputHandler}
-      ></LoginInput>
-      <h1>{userData.name}</h1>
-      <div className="apiCallButton" onClick={buttonHamdler}></div>
-      <div className="UserContainer">
-        {characters.length > 0 ? (
-          <>
-            {characters.map((char) => {
-              return (
-                <ArtistCard
-                  id={char.id}
-                  image={char.image}
-                  name={char.name}
-                ></ArtistCard>
-              );
-            })}
-          </>
-        ) : null}
+    <>
+      <div className="topHome">
+        <div className="logo"></div>
       </div>
-    </div>
+    </>
   );
 };
