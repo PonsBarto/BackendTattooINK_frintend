fd5c6e06474274eaa55c4ddf6a7bc101bfa84125
Barto <ponsbarto@gmail.com>
2024-03-10 12:35:35 +0100

    Habilitar la creación de citas con selección de artistas desde un menú desplegable, asociando el ID del artista seleccionado en lugar del ID del usuario.

--- src/Pages/Appointments/Appointments.jsx
+++ src/Pages/Appointments/Appointments.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Appointments.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { createAppointment } from "../../Services/apiCalls";
+import { createAppointment, bringAllArtists } from "../../Services/apiCalls"; // Asegúrate de importar bringAllArtists desde tus apiCalls
 import { userData } from "../userSlice";
 import { CustomInput } from "../../Components/CustomInput/CustomInput";
 import { jwtDecode } from "jwt-decode";
 
 export const Appointments = () => {
   const userRdxData = useSelector(userData);
-
   const myId = userRdxData.credentials.userData.userId;
   const [newAppointment, setNewAppointment] = useState({
     user_id: myId,
@@ -17,10 +16,19 @@ export const Appointments = () => {
     date: "",
     time: "",
   });
-  console.log(myId)
+
+  const [artists, setArtists] = useState([]);
+  
+  useEffect(() => {
+    if (artists.length === 0) {
+      bringAllArtists().then((arts) => {
+        console.log("Artists:", arts); // Imprime los datos de artists
+        setArtists(arts);
+      });
+    }
+  }, []);
 
   const dispatch = useDispatch();
-
   const navigate = useNavigate();
 
   const inputHandler = (event) => {
@@ -29,69 +37,67 @@ export const Appointments = () => {
       [event.target.name]: event.target.value,
     }));
   };
-  console.table(event.target.value)
-// const token = userRdxData.credentials.token
-const buttonHandler = () => {
-  const token = userRdxData.credentials.token;
-  console.log(newAppointment)
-  if (!token) {
-    // Si el token no está presente, maneja el caso apropiadamente, por ejemplo, redireccionando al usuario a la página de inicio de sesión
-    // navigate("/login");
-    return;
-  }
 
-  createAppointment(token, newAppointment)
-    .then((res) => {
-      console.log(res)
-      const decodedToken = jwtDecode(token);
-      const data = {
-        token: token,
-        userData: decodedToken,
-      };
+  const buttonHandler = () => {
+    const token = userRdxData.credentials.token;
+    if (!token) {
+      // Si el token no está presente, maneja el caso apropiadamente, por ejemplo, redireccionando al usuario a la página de inicio de sesión
+      // navigate("/login");
+      return;
+    }
 
-      // Asegúrate de despachar la acción correcta, si es necesario
-      // dispatch(Appointments({}));
+    createAppointment(token, newAppointment)
+      .then((res) => {
+        console.log(res)
+        const decodedToken = jwtDecode(token);
+        const data = {
+          token: token,
+          userData: decodedToken,
+        };
 
-      setTimeout(() => {
-        navigate("/profile");
+        // Asegúrate de despachar la acción correcta, si es necesario
+        // dispatch(Appointments({}));
+
+        setTimeout(() => {
+          navigate("/profile");
+        });
+      })
+      .catch((err) => {
+        console.error("Ha ocurrido un error", err);
+        // Aquí podrías proporcionar retroalimentación al usuario o realizar otras acciones según el error
       });
-    })
-    .catch((err) => {
-      console.error("Ha ocurrido un error", err);
-      // Aquí podrías proporcionar retroalimentación al usuario o realizar otras acciones según el error
-    });
-};
-
-    return (
-        <div className="body">
-        <div className="requiredFields">
-          {/* <CustomInput
-            placeholder={"user_id"}
-            type={"number"}
-            name={"user_id"}
-            handler={inputHandler}
-          ></CustomInput> */}
-          <CustomInput
-            placeholder={"artist_id"}
-            type={"number"}
-            name={"artist_id"}
-            handler={inputHandler}
-          ></CustomInput>
-          <CustomInput
-            placeholder={"date"}
-            type={"date"}
-            name={"date"}
-            handler={inputHandler}
-          ></CustomInput>
-          <CustomInput
-            placeholder={"time"}
-            type={"time"}
-            name={"time"}
-            handler={inputHandler}
-          ></CustomInput>
-          <input type="submit" onClick={buttonHandler} value="Register"></input>
-        </div>
-      </div>
-    );
-
   };
+
+  return (
+    <div className="body">
+      <div className="requiredFields">
+        <label htmlFor="artist_id">Select Artist:</label>
+        <select
+          name="artist_id"
+          value={newAppointment.artist_id}
+          onChange={inputHandler}
+        >
+          <option value="">Select an artist</option>
+          {artists.map((artist) => (
+            <option key={artist.id} value={artist.id}>
+              {artist.name}
+            </option>
+          ))}
+        </select>
+        <CustomInput
+          placeholder={"date"}
+          type={"date"}
+          name={"date"}
+          handler={inputHandler}
+        />
+        <CustomInput
+          placeholder={"time"}
+          type={"time"}
+          name={"time"}
+          handler={inputHandler}
+        />
+        <input type="submit" onClick={buttonHandler} value="Register" />
+      </div>
+    </div>
+  );
+};
--- src/Pages/Artist/Artist.jsx
+++ src/Pages/Artist/Artist.jsx
@@ -2,39 +2,36 @@ import { useEffect, useState } from "react";
 import { ArtistCard } from "../../Components/ArtistCard/ArtistCard";
 import { bringAllArtists } from "../../Services/apiCalls";
 import "./Artists.css";
-// import { useNavigate } from "react-router-dom";
 
 export const Artists = () => {
   const [artists, setArtists] = useState([]);
 
   useEffect(() => {
     if (artists.length === 0) {
-      bringAllArtists().then((arts) => {
-        setArtists(arts);
+      bringAllArtists().then((data) => {
+        setArtists(data); // Establece los datos directamente
       });
     }
   }, []);
 
   return (
     <>
-      {/* <div className="body"> */}
-        <h1 className="team-title">Meet the team</h1>
-        <div className="artistContainer">
-          {artists.length > 0 ? (
-            artists.map((artist) => {
-              return (
-                <ArtistCard
-                  key={artist.id}
-                  name={artist.name}
-                  photo={artist.photo}
-                />
-              );
-            })
-          ) : (
-            <p>No hay artistas para mostrar.</p>
-          )}
-        </div>
-      {/* </div> */}
+      <h1 className="team-title">Meet the team</h1>
+      <div className="artistContainer">
+        {artists && artists.length > 0 ? (
+          artists.map((artist) => {
+            return (
+              <ArtistCard
+                key={artist.id}
+                name={artist.name}
+                photo={artist.photo}
+              />
+            );
+          })
+        ) : (
+          <p>No hay artistas para mostrar.</p>
+        )}
+      </div>
     </>
   );
 };
