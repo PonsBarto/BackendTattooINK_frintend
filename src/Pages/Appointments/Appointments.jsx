481f7ff30855aefddbcdaea41a3cb46a5017b20e
Barto <ponsbarto@gmail.com>
2024-03-10 17:03:18 +0100

    se agregó una función para actualizar los detalles del perfil

--- src/Pages/Appointments/Appointments.jsx
+++ src/Pages/Appointments/Appointments.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./Appointments.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { createAppointment, bringAllArtists } from "../../Services/apiCalls"; // Asegúrate de importar bringAllArtists desde tus apiCalls
+import { createAppointment, bringAllArtists } from "../../Services/apiCalls";
 import { userData } from "../userSlice";
 import { CustomInput } from "../../Components/CustomInput/CustomInput";
 import { jwtDecode } from "jwt-decode";
+import { Form, Button } from "react-bootstrap";
 
 export const Appointments = () => {
   const userRdxData = useSelector(userData);
@@ -16,13 +17,12 @@ export const Appointments = () => {
     date: "",
     time: "",
   });
-
   const [artists, setArtists] = useState([]);
   
   useEffect(() => {
     if (artists.length === 0) {
       bringAllArtists().then((arts) => {
-        console.log("Artists:", arts); // Imprime los datos de artists
+        console.log("Artists:", arts);
         setArtists(arts);
       });
     }
@@ -41,22 +41,18 @@ export const Appointments = () => {
   const buttonHandler = () => {
     const token = userRdxData.credentials.token;
     if (!token) {
-      // Si el token no está presente, maneja el caso apropiadamente, por ejemplo, redireccionando al usuario a la página de inicio de sesión
-      // navigate("/login");
       return;
     }
 
     createAppointment(token, newAppointment)
       .then((res) => {
-        console.log(res)
+        console.log(res);
         const decodedToken = jwtDecode(token);
         const data = {
           token: token,
           userData: decodedToken,
         };
 
-        // Asegúrate de despachar la acción correcta, si es necesario
-        // dispatch(Appointments({}));
 
         setTimeout(() => {
           navigate("/profile");
@@ -64,39 +60,54 @@ export const Appointments = () => {
       })
       .catch((err) => {
         console.error("Ha ocurrido un error", err);
-        // Aquí podrías proporcionar retroalimentación al usuario o realizar otras acciones según el error
       });
   };
 
   return (
-    <div className="body">
-      <div className="requiredFields">
-        <label htmlFor="artist_id">Select Artist:</label>
-        <select
-          name="artist_id"
-          value={newAppointment.artist_id}
-          onChange={inputHandler}
-        >
-          <option value="">Select an artist</option>
-          {artists.map((artist) => (
-            <option key={artist.id} value={artist.id}>
-              {artist.name}
-            </option>
-          ))}
-        </select>
-        <CustomInput
-          placeholder={"date"}
-          type={"date"}
-          name={"date"}
-          handler={inputHandler}
-        />
-        <CustomInput
-          placeholder={"time"}
-          type={"time"}
-          name={"time"}
-          handler={inputHandler}
-        />
-        <input type="submit" onClick={buttonHandler} value="Register" />
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <Form className="mt-5">
+            <Form.Group controlId="artist_id">
+              <Form.Label>Select your Artist:</Form.Label>
+              <Form.Control
+                as="select"
+                name="artist_id"
+                value={newAppointment.artist_id}
+                onChange={inputHandler}
+              >
+                <option value="">Select an artist</option>
+                {artists.map((artist) => (
+                  <option key={artist.id} value={artist.id}>
+                    {artist.name}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="date">
+              <Form.Label>Date:</Form.Label>
+              <Form.Control
+                type="date"
+                name="date"
+                value={newAppointment.date}
+                onChange={inputHandler}
+              />
+            </Form.Group>
+            <Form.Group controlId="time">
+              <Form.Label>Time:</Form.Label>
+              <Form.Control
+                type="time"
+                name="time"
+                value={newAppointment.time}
+                onChange={inputHandler}
+              />
+            </Form.Group>
+
+            <Button variant="primary" onClick={buttonHandler}>
+              Confirm
+            </Button>
+          </Form>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,77 +1,150 @@
 import "./Profile.css";
-import { bringProfile } from "../../Services/apiCalls";
+import { bringProfile, updateProfile } from "../../Services/apiCalls";
+
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { userData } from "../userSlice";
-import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
-
+import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 
 export const Profile = () => {
   const navigate = useNavigate();
-
   const [profileData, setProfileData] = useState({});
-
   const userRdxData = useSelector(userData);
   const token = userRdxData.credentials.token;
-
   const myId = userRdxData.credentials.userData.userId;
-  console.log(userRdxData);
+
+  const [editMode, setEditMode] = useState(false);
+  const [editableData, setEditableData] = useState({});
 
   useEffect(() => {
     if (!token) {
       navigate("/register");
     } else {
-      setTimeout(() => {
-        // setTimeout para hacer más amable el acceso a los datos de perfil
-        bringProfile(token, myId).then((res) => {
-          console.log(res, "soy la respuesta del server");
-          setProfileData(res);
-        });
-      }, 2000);
+      bringProfile(token, myId).then((res) => {
+        setProfileData(res);
+        setEditableData(res); // Inicializar los datos editables con los datos del perfil
+      });
     }
   }, []);
 
-  useEffect(() => {
-    console.log(profileData);
-  }, [profileData]);
+  const inputHandler = (event) => {
+    setEditableData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const buttonHandler = () => {
+    if (editMode) {
+      // Pasamos el ID del usuario junto con los datos actualizados
+      updateProfile(token, myId, editableData).then((updatedProfile) => {
+        setProfileData(updatedProfile);
+        setEditMode(false);
+      }).catch((error) => {
+        console.error("Error updating profile:", error);
+        // Manejar errores, por ejemplo, mostrar un mensaje de error al usuario
+      });
+    } else {
+      setEditMode(true);
+    }
+  };
+  
 
   return (
     <>
       <div className="body">
-          {!!profileData.phone_number ? (
-            <>
-    <Container className="mt-5">
-    <Card.Title className="profile-card-title">Welcome {profileData.name} {profileData.last_name}</Card.Title> {/* Aplica la clase CSS al título */}
-      <Row>
-        {/* Lado Izquierdo: Imagen */}
-        <Col md={5} className="mt-md-4 text-center mx-auto">
-          <img src={profileData.photo} className="img-fluid" alt="Imagen de perfil" />
-        </Col>
-        {/* Lado Derecho: Detalles del perfil */}
-        <Col md={7} className="mt-md-4">
-          <Card className="profile-card"> {/* Aplica la clase CSS al componente Card */}
-            <Card.Body>
-              <ul className="list-group list-group-flush">
-              <li className="list-group-item">Name: {profileData.name}</li>
-              <li className="list-group-item">Last Name: {profileData.last_name}</li>
-              <li className="list-group-item">Email: {profileData.email}</li>
-              <li className="list-group-item">Phone number: {profileData.phone_number}</li>
-              <li className="list-group-item">Address: {profileData.address}</li>
-              </ul>
-              <Button variant="primary" className="mt-3">Editar</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-
-    
-            </>
-          ) : (
-            <p>Cargando datos de perfil...</p>
-          )}{" "}
+        {!!profileData.phone_number ? (
+          <>
+            <Container className="mt-5">
+              <Card.Title className="profile-card-title">
+                Welcome {profileData.name} {profileData.last_name}
+              </Card.Title>{" "}
+              {/* Aplica la clase CSS al título */}
+              <Row>
+                {/* Lado Izquierdo: Imagen */}
+                <Col md={5} className="mt-md-4 text-center mx-auto">
+                  <img
+                    src={profileData.photo}
+                    className="img-fluid"
+                    alt="Imagen de perfil"
+                  />
+                </Col>
+                {/* Lado Derecho: Detalles del perfil */}
+                <Col md={7} className="mt-md-4">
+                  <Card className="profile-card">
+                    {" "}
+                    {/* Aplica la clase CSS al componente Card */}
+                    <Card.Body>
+                      <ul className="list-group list-group-flush">
+                        <li className="list-group-item">
+                          Name: {editMode ? (
+                            <Form.Control
+                              type="text"
+                              name="name"
+                              value={editableData.name}
+                              onChange={inputHandler}
+                            />
+                          ) : (
+                            profileData.name
+                          )}
+                        </li>
+                        <li className="list-group-item">
+                          Last Name: {editMode ? (
+                            <Form.Control
+                              type="text"
+                              name="last_name"
+                              value={editableData.last_name}
+                              onChange={inputHandler}
+                            />
+                          ) : (
+                            profileData.last_name
+                          )}
+                        </li>
+                        <li className="list-group-item">
+                          Email: {profileData.email}
+                        </li>
+                        <li className="list-group-item">
+                          Phone number: {editMode ? (
+                            <Form.Control
+                              type="text"
+                              name="phone_number"
+                              value={editableData.phone_number}
+                              onChange={inputHandler}
+                            />
+                          ) : (
+                            profileData.phone_number
+                          )}
+                        </li>
+                        <li className="list-group-item">
+                          Address: {editMode ? (
+                            <Form.Control
+                              type="text"
+                              name="address"
+                              value={editableData.address}
+                              onChange={inputHandler}
+                            />
+                          ) : (
+                            profileData.address
+                          )}
+                        </li>
+                      </ul>
+                      <Button
+                        variant="primary"
+                        className="mt-3"
+                        onClick={buttonHandler}
+                      >
+                        {editMode ? "Save" : "Update details"}
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </>
+        ) : (
+          <p>Cargando datos de perfil...</p>
+        )}{" "}
       </div>
     </>
   );
