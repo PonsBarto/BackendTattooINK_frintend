51d10725bebcf8eed66ca88b31d09d4554cbedab
Barto <ponsbarto@gmail.com>
2024-03-11 09:46:04 +0100

    Se actualizaron los colores de fuente y se modificó el diseño de inicio de sesión y registro.

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { login, userData } from "../userSlice";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { userSignUp, userLogin } from "../../Services/apiCalls";
+import "./Register.css"
 
 export const Register = () => {
   const [signUpData, setSignUpData] = useState({
@@ -55,79 +56,82 @@ export const Register = () => {
   };
 
   return (
-    <div className="container mt-3" id="signUpBox">
-      <h1>Register</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter name"
-            name="name"
-            value={signUpData.name}
-            onChange={inputHandler}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter last name"
-            name="last_name"
-            value={signUpData.last_name}
-            onChange={inputHandler}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formAddress">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter address"
-            name="address"
-            value={signUpData.address}
-            onChange={inputHandler}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            value={signUpData.email}
-            onChange={inputHandler}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            name="password"
-            value={signUpData.password}
-            onChange={inputHandler}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formPhoneNumber">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter phone number"
-            name="phone_number"
-            value={signUpData.phone_number}
-            onChange={inputHandler}
-            required
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Register
-        </Button>
-      </Form>
-    </div>
+    <Container className="mt-5">
+      <div id="signUpBox" className="bg-light p-4 rounded">
+        <h1 className="mb-4">Register</h1>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              name="name"
+              value={signUpData.name}
+              onChange={inputHandler}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter last name"
+              name="last_name"
+              value={signUpData.last_name}
+              onChange={inputHandler}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter address"
+              name="address"
+              value={signUpData.address}
+              onChange={inputHandler}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              value={signUpData.email}
+              onChange={inputHandler}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              name="password"
+              value={signUpData.password}
+              onChange={inputHandler}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formPhoneNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter phone number"
+              name="phone_number"
+              value={signUpData.phone_number}
+              onChange={inputHandler}
+              required
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="w-100">
+            Register
+          </Button>
+        </Form>
+        <p className="mt-3">Already have an account? <a href="/login">login</a></p>
+      </div>
+    </Container>
   );
 };
