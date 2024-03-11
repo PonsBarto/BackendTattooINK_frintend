e6bafddc90907549f8ef01f5d2b5308f3efbcb67
Barto <ponsbarto@gmail.com>
2024-03-11 09:06:24 +0100

    Se modificó la vista donde el superadministrador puede listar usuarios y citas.

--- src/Pages/AllAppointments/AllAppointments.jsx
+++ src/Pages/AllAppointments/AllAppointments.jsx
@@ -1,27 +1,64 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { userData } from "../userSlice";
 import { bringAllAppointments } from "../../Services/apiCalls";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import "./AllAppointments.css";
 
+export const AllAppointments = () => {
+  const [appointments, setAppointments] = useState([]);
+  const userRdxData = useSelector(userData);
+  const token = userRdxData.credentials.token;
 
-export const AllAppointments =  () => {
-    const [appointments, setAppointments] = useState([]);
-    const userRdxData = useSelector(userData);
-    const token = userRdxData.credentials.token;
+  useEffect(() => {
+    if (appointments.length === 0) {
+      bringAllAppointments(token)
+        .then((res) => {
+          setAppointments(res.results);
+        })
+        .catch((error) => {
+          console.error("Error fetching appointments:", error);
+        });
+    }
+  }, [appointments, token]);
 
-    useEffect(() => {
-            bringAllAppointments(token, appointments)
-                .then((appointments) => {
-                    setAppointments(appointments);
-                })
-                .catch((error) => {
-                    console.error("Error fetching appointments:", error);
-                });
-
-    }, [token]);
-    console.log(appointments)
-
-
-
-    return ('appointments aqui')
-}
+  return (
+    <Container>
+      <h1 className="text-center mt-5 mb-4">Your Appointments</h1>
+      <Row xs={1} md={2} lg={3} className="g-4">
+        {appointments && appointments.length > 0 ? (
+          appointments.map((appointment) => (
+            <Col
+              key={`${appointment.id}-${appointment.date}-${appointment.time}`}
+            >
+              <Card className="shadow-sm appointment-card" id="custom-card">
+                <Card.Body>
+                  <Card.Title className="text-center fs-5">
+                    Artist: {appointment.artist_name}
+                  </Card.Title>
+                  <hr />
+                  <div className="text-center">
+                    <p>
+                      <strong>Date:</strong> {appointment.date}
+                    </p>
+                    <p>
+                      <strong>Time:</strong> {appointment.time}
+                    </p>
+                    <p>
+                      <strong>Customer:</strong> {appointment.user_name}{" "}
+                      {appointment.user_last_name}
+                    </p>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))
+        ) : (
+          <Col>
+            <p className="text-center">No appointments available.</p>
+          </Col>
+        )}
+      </Row>
+    </Container>
+  );
+};
